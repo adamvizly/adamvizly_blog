7ee94684aeb61c39319450076ecd829a83fb9394
Ali Asghari <asghari.ali10@gmail.com>
2020-09-23 08:18:34 -0700

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -2,12 +2,15 @@ $('body').terminal({
     list: function() {
         this.echo('Hello -> jafar' +
                   '. Wellcome to this terminal.');
-    },
+    }
+}, {
+    greetings: 'Welcome; I am Ali Asghari '.\n.'use list to see list of commands.'
+});
+
+$('body').terminal({
     cat: function(width, height) {
         const img = $('<img src="https://placekitten.com/' +
                       width + '/' + height + '">');
         this.echo(img);
     }
-}, {
-    greetings: 'Welcome; I am Ali Asghari '.\n.'use list to see list of commands.'
 });
