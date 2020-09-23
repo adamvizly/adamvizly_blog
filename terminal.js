cca3cf7508f54241022a933ae7be604da1c86743
Ali Asghari <asghari.ali10@gmail.com>
2020-09-23 07:55:08 -0700

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -1,23 +1,8 @@
-figlet.defaults({fontPath: 'https://unpkg.com/figlet@1.4.0/fonts/'});
-figlet.preloadFonts(["Standard", "Slant"], ready);
-
-var term;
-
-function ready() {
-   term = $('body').terminal(function(cmd) {
-        this.echo(() => render(cmd));
-    }, {
-        greetings: function() {
-            return render('Welcome', 'Slant') +
-                `\n[[;rgba(255,255,255,0.99);] I am Ali Asghari].\n`;
-        }
-    });
-}
-
-function render(text, font) {
-    return figlet.textSync(text, {
-        font: font || 'Standard',
-        width: !term ? 80 : term.cols(),
-        whitespaceBreak: true
-    });
-}
+$('body').terminal({
+    hello: function(what) {
+        this.echo('Hello, ' + what +
+                  '. Wellcome to this terminal.');
+    }
+}, {
+    greetings: 'My First Terminal'
+});
